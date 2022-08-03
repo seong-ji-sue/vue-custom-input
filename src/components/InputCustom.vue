f52aeab76bbd6cbd518ab2619ef348930256957d
jisue <tjdwltn121@gmail.com>
2022-08-04 02:37:23 +0900

    레이아웃 구성 및 데이터 바인딩 성공 css나 검색 취소 기능 넣을꺼
    (cherry picked from commit 0331f5451095f2495ec731254001009fa3ce8131)

---
+++ src/components/InputCustom.vue
@@ -0,0 +1,98 @@
+<template>
+  <div>
+    <label>
+      {{label}}
+      <input
+          type="text"
+          :name="name"
+          v-model="innerValue"
+          :placeholder="showPlaceholderText"
+          @change="onChange"
+          @focus="onFocus"
+          @blur="onBlur"
+          :style="{width:`${width}px`,height:`${height}`}"
+          :class="{'click-border':isFocus}"
+          class="input-style"
+      />
+    </label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "InputCustom",
+  emits:["change","on-focus"],
+  props: {
+    value: { //input 이벤트로 적은값
+      type: [Number,String],
+    },
+    label:{
+      type:String,
+      default:undefined
+    },
+    placeholder: {
+      type: String,
+      default: undefined
+    },
+    width:{
+      type:String,
+      default:''
+    },
+    height:{
+      type:String,
+      default:''
+    }
+
+  },
+  data() {
+    return {
+      innerValue: '',//input 값
+      isFocus: false //input을 클릭했는지 여부
+
+    }
+  },
+  computed: {
+    name() {
+      return this.label.toLowerCase();
+    },
+    showPlaceholderText(){
+      return !this.isFocus ? this.placeholder : ''
+    }
+  },
+  methods: {
+    /**
+     * input을 클릭할때
+     */
+    onFocus() {//TODO::input을 클릭했을때 스타일 먹이고 다른쪽 클릭할때 스타일이 없어져야됨
+      this.isClick = true
+    },
+    onBlur(){
+      this.isClick = false
+    },
+    /**
+     * input 값을 입력했을때
+     */
+    onChange() {
+      if(this.innerValue === this.value) return
+      this.$emit("input",this.innerValue)
+      this.innerValue =undefined
+
+    }
+
+  },
+}
+</script>
+
+<style scoped>
+.init-style{
+  border: 0;
+}
+.click-border{
+  border:2px solid red !important;
+}
+.input-style{
+  border-radius: 5px;
+  font-size: 13px;
+  color: #808080;
+}
+</style>
